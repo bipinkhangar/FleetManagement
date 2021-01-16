0eeea4af4b00f61e10febc7ae6306a9b16bafe6f
bipinkhangar <61385129+bipinkhangar@users.noreply.github.com>
2021-01-16 17:58:04 +0530

    Delete Fleet-Management-backend directory

--- Fleet-Management-backend/AmmenitiesBoot/AmmenitiesRepository/AmmenitiesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.model.Ammenities;
-
-public interface AmmenitiesRepository extends CrudRepository<Ammenities,Integer> {
-
-}
